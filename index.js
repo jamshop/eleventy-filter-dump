3c89b1731e8ad6b8f99306d560fe5cd1ea6c0053
MadeByMike <mike@madebymike.com.au>
2020-12-14 00:54:58 +1100

    Fix an issue where HTML in the debug result can cause malformed HTML

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const util = require('util');
 const ansiHTML = require('ansi-html');
 
 module.exports = (object) => {
-  const content = `<pre style="background: black; color: #fff; padding: 5px;">${ansiHTML(util.inspect(object, { colors:true }))}</pre>`;
-  return `<script>
-(function(){
-  const content = document.createElement('div');
-  document.body.prepend(content);
-  content.innerHTML = ${JSON.stringify(content)}
-})();
-</script>`;
+  // Let's be sure to escape HTML characters in the util.inspect results
+  const safeContent = util.inspect(object, { colors:true })
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+
+  // It's really important that the HTML strings are on one line. 
+  // I've seen instances of some 11ty templates converting new lines into paragraphs
+  const containerStyles = `background: black; color: #fff; padding: 5px;`
+  const content = `<pre style="${containerStyles}">${ansiHTML(safeContent)}</pre>`;
+  return `<script>(function(){const content = document.createElement('div');document.body.prepend(content);content.innerHTML = ${JSON.stringify(content)};})();</script>`;
 }
